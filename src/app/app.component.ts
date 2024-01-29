3622340884aeef19f02b38ec21d532c716995675
Lasha Nuroshvili <lasha.nuroshvili.1@btu.edu.ge>
2024-01-29 21:55:05 +0400

    Added bootstrap, adding header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./header/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'famous_quote_quizz_front';
+  title = 'Famous Quote Quiz';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { IndexComponent} from "./admin/index.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin', component: IndexComponent
+  }
+];
